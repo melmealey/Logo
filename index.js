329ac1136e4adb554051c9c90b6cd5552fb7e226
Mel Mel Mom <melmelmomof4@gmail.com>
2024-02-05 13:43:32 -0500

    wokred on .then response in index.js

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
- // Runs the application using imports from lib/
+// Runs the application using imports from lib/
 //importing the inquirer library
 const inquirer = require('inquirer');
 //importing the file system
 const fs = require('fs');
 
-const prompts = async () => {
-  try {
-    const input = await inquirer.prompt ({
-    
-        type: 'list',
+const { Circle, Square, Triangle } = require('./lib/shapes');
+
+inquirer.prompt ([
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose the shape you would like for your logo:',
+      choices: ['Circle', 'Square', 'Triangle'],
+    },
+      {
+        type: 'input',
         name: 'shape',
-        message: 'Choose the shape you would like for your logo:',
-        choices: ['Circle', 'Square', 'Triangle'],
+        message: 'SHAPE COLOR: : Enter a color for your shape:',
       },
       {
-      type: 'input',
-      name: 'text',
-      message: 'TEXT: Enter up to 3 Characters:',
+        type: 'input',
+        name: 'text',
+        message: 'TEXT: Enter up to 3 Characters:',
       },
       {
         type: 'input',
         name: 'text-color',
-        message: 'TEXT COLOR: Enter a color:',
+        message: 'TEXT COLOR: Enter a text color:',
       },
-      {
-        type: 'input',
-        name: 'shape',
-        message: 'SHAPE COLOR: : Enter a color:',
-       },
-      }
+    ])
+  
+ .then(response => {
 
+    if (response.shape === 'circle') {
+      const circle = new Circle()
+      circle.setTextColor(response.setTextColor)
+      circle.setShapeColor(response.setShapeColor)
+      circle.setText(response.text)
+      fs.writeFileSync('.Assets/circle.svg', circle.render())
     }
- 
+    if (response.shape === 'square') {
+      const square = new Square()
+      circle.setTextColor(response.setTextColor)
+      circle.setShapeColor(response.setShapeColor)
+      circle.setText(response.text)
+      fs.writeFileSync('.Assets/square.svg', square.render())
+    }
+    if (response.shape === 'triangle') {
+      const triangle = new Triangle()
+      circle.setTextColor(response.setTextColor)
+      circle.setShapeColor(response.setShapeColor)
+      circle.setText(response.text)
+      fs.writeFileSync('.Assets/triangle.svg', triangle.render())
+    }
+  })
 
 
-    const response = await fetch(`https://api.github.com/users/${input.username}`)
-    const json = await response.json()
+const response = await fetch(`https://api.github.com/users/${input.username}`)
+const json = await response.json()
 
-    console.log(json)
-   catch (err) {
-    console.log(err)
-  }
+console.log(json)
+//    catch (err) {
+//   console.log(err)
+// }
 
 
 prompts()
