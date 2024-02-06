67a037e0fbc5dc217c323f2a598580883877db20
Mel Mel Mom <melmelmomof4@gmail.com>
2024-02-06 13:41:20 -0500

    code running and creating unique svg files

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ inquirer.prompt ([
     },
       {
         type: 'input',
-        name: 'shape',
+        name: 'shapeColor',
         message: 'SHAPE COLOR: : Enter a color for your shape:',
       },
       {
@@ -25,7 +25,7 @@ inquirer.prompt ([
       },
       {
         type: 'input',
-        name: 'text-color',
+        name: 'textColor',
         message: 'TEXT COLOR: Enter a text color:',
       },
     ])
@@ -33,38 +33,29 @@ inquirer.prompt ([
  .then(response => {
 console.log(response)
 
-    if (response.shape === 'circle') {
+    if (response.shape === 'Circle') {
       const circle = new Circle()
-      circle.setTextColor(response.setTextColor)
-      circle.setShapeColor(response.setShapeColor)
+      circle.setTextColor(response.textColor)
+      circle.setShapeColor(response.shapeColor)
       circle.setText(response.text)
-      fs.writeFileSync('.Assets/circle.svg', circle.render())
+      fs.writeFileSync('./Assets/circle.svg', circle.render())
     }
-    if (response.shape === 'square') {
-      const square = new Square()
-      circle.setTextColor(response.setTextColor)
-      circle.setShapeColor(response.setShapeColor)
-      circle.setText(response.text)
-      fs.writeFileSync('.Assets/square.svg', square.render())
-    }
-    if (response.shape === 'triangle') {
-      const triangle = new Triangle()
-      circle.setTextColor(response.setTextColor)
-      circle.setShapeColor(response.setShapeColor)
-      circle.setText(response.text)
-      fs.writeFileSync('.Assets/triangle.svg', triangle.render())
-    }
-  })
+    
+    if (response.shape === 'Square') {
+    const square = new Square();
+    square.setTextColor(response.textColor);
+    square.setShapeColor(response.shapeColor);
+    square.setText(response.text);
+    fs.writeFileSync('./Assets/square.svg', square.render());
+  }
 
-
-// console.log()
-//    catch (err) {
-//   console.log(err)
-// }
-
-
-// prompts()
-
-// module.exports = Shapes
+  if (response.shape === 'Triangle') {
+    const triangle = new Triangle();
+    triangle.setTextColor(response.textColor);
+    triangle.setShapeColor(response.shapeColor);
+    triangle.setText(response.text);
+    fs.writeFileSync('./Assets/triangle.svg', triangle.render());
+  }
+});
 
 
